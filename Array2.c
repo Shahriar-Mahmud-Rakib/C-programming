367e502ad4ca737d5c50495800bbb7ceaba94161
Rakib <smrakib.cse.pust@gmail.com>
2023-10-30 12:30:51 +0600

    Add files via upload

---
+++ Array2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int arr[5]={1,2,3,4,5};
+printf("arr[0] = %d\n", arr[0]);
+printf("arr[1] = %d\n", arr[1]);
+printf("arr[2] = %d\n", arr[2]);
+printf("arr[3] = %d\n", arr[3]);
+printf("arr[4] = %d\n", arr[4]);
+return 0;
+}
---
+++ Even Odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+int number;
+printf("enter the number u want:\t");
+scanf("%d",&number);
+if(number%2==0)
+{
+printf("the number is even \n");
+}
+else
+{
+printf("the number is odd \n");
+}
+return 0;
+}
